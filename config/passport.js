cbd128944a318c2e9ce4c1ee4e4edc002389aca3
Akhmad Khuzeiron <128360994+ahmadkhuzeiron@users.noreply.github.com>
2023-06-06 14:56:47 +0700

    passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,28 +1,26 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
 const User = require('../models/user');
 const sequelize = require('../database/connection');
 
-
-passport.use(new LocalStrategy(
-  async (username, password, done) => {
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
     try {
       const user = await User.findOne({ where: { username } });
-
       if (!user) {
         return done(null, false, { message: 'Incorrect username.' });
       }
-
-      if (user.password !== password) {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
         return done(null, false, { message: 'Incorrect password.' });
       }
-
       return done(null, user);
     } catch (error) {
       return done(error);
     }
-  }
-));
+  })
+);
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -36,5 +34,3 @@ passport.deserializeUser(async (id, done) => {
     done(error);
   }
 });
-
-module.exports = passport;
